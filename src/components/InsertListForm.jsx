89a8569b1353db801ef4b352256b1aebb93637eb
Sancky <54481033+Sancky@users.noreply.github.com>
2023-11-13 23:21:39 +0200

    Initial commit

---
+++ src/components/InsertListForm.jsx
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default function InsertListForm({ handleList }) {
+	function submitTodoItem(e) {
+		if (!e.target.value.length || e.key !== "Enter") return;
+
+		const description = e.target.value;
+
+		handleList((prevState) => [
+			...prevState,
+			{ id: uuidv4(), description: description, isEditing: false },
+		]);
+
+		e.target.value = "";
+	}
+
+	return (
+		<>
+			<div className="flex flex-wrap items-center justify-center bg-slate-800 p-1.5 m-1 w-[50%] rounded">
+				<input
+					className="flex flex-wrap items-center justify-between bg-slate-600 text-slate-900 font-light text-1xl text-center p-1.5 m-1 w-full rounded outline-none placeholder:text-slate-900 selection:bg-slate-800 selection:text-slate-600"
+					placeholder="Write your new todo task here..."
+					onKeyDown={submitTodoItem}
+					name="insert-item-description"
+				></input>
+			</div>
+		</>
+	);
+}
---
+++ src/components/ShowList.jsx
@@ -0,0 +1,15 @@
+import InsertListItem from "./InsertListItem";
+
+export default function ShowList({ list, handleList }) {
+	if (!list.length) return null;
+
+	return (
+		<>
+			<div className="flex flex-col flex-wrap content-center bg-slate-800 rounded-md p-2 m-6 w-[50%]">
+				{list.map((element, index) => (
+					<InsertListItem item={element} handleList={handleList} key={index}/>
+				))}
+			</div>
+		</>
+	);
+}
